f96bc522fe353bbb4c4b937c600ed4a6ac5cf5ad
David Drughi <drughidavid6@gmail.com>
2020-02-03 17:38:43 +0100

    secret key

--- django_project/settings.py
+++ django_project/settings.py
@@ -1,6 +1,5 @@
 
 
-
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -14,7 +13,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'exhlfdat&vfum(-34*c2uroi(($ww(yo$9pv98=e6p^gl(-eoj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://issuetracker2.herokuapp.com/']
 
---
+++ env.py
@@ -0,0 +1,8 @@
+import os
+
+
+os.environ.setdefault("SECRET_KEY", "super_ultra_mega_secret_key")
+
+
+EMAIL_USER = ["vasile.test.user@gmail.com"]
+EMAIL_PASS = ["asdy oexu xpwz moic"]
